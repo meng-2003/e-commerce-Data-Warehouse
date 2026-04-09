c8620d113ea53a28b5b01d446bff3ded3f4e4edf
lxj <18033116818@163.com>
2026-04-09 20:53:48 +0800

    DWD 6 交易域支付成功事务事实表

--- realtime-common/src/main/java/com/lxj/gmall/realtime/common/constant/Constant.java
+++ realtime-common/src/main/java/com/lxj/gmall/realtime/common/constant/Constant.java
@@ -33,7 +33,7 @@ public class Constant {
 
     public static final String TOPIC_DWD_TRADE_ORDER_CANCEL_DETAIL = "dwd_trade_order_cancel_detail";
 
-    public static final String TOPIC_DWD_TRADE_ORDER_PAYMENT_SUCCESS = "dwd_trade_order_payment_success";
+    public static final String TOPIC_DWD_TRADE_ORDER_PAYMENT_SUCCESS_DETAIL = "dwd_trade_order_payment_success_detail";
     public static final String TOPIC_DWD_TRADE_ORDER_REFUND = "dwd_trade_order_refund";
 
     public static final String TOPIC_DWD_TRADE_REFUND_PAYMENT_SUCCESS = "dwd_trade_refund_payment_success";
