48684616dea94bebede7cbe52375f5892d6d6db5
lxj <18033116818@163.com>
2026-04-11 16:05:49 +0800

    DWS 7 交易域支付成功各窗口汇总表

---
+++ realtime-common/src/main/java/com/lxj/gmall/realtime/common/bean/TradePaymentBean.java
@@ -0,0 +1,28 @@
+package com.lxj.gmall.realtime.common.bean;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+/**
+ * @author Laoxingjie
+ * @description 支付成功实体类
+ * @create 2026/4/11 16:01
+ **/
+
+@Data
+@AllArgsConstructor
+public class TradePaymentBean {
+    // 窗口起始时间
+    String stt;
+    // 窗口终止时间
+    String edt;
+    // 当天日期
+    String curDate;
+    // 支付成功独立用户数
+    Long paymentSucUniqueUserCount;
+    // 支付成功新用户数
+    Long paymentSucNewUserCount;
+    // 时间戳
+    @JSONField(serialize = false)
+    Long ts;
+}
