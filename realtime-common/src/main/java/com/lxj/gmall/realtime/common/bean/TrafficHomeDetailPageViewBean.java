fbd997c49c678cc194b6bf9790c7a385d74a6019
lxj <18033116818@163.com>
2026-04-11 14:40:07 +0800

    DWS 3 流量域首页、详情页页面浏览各窗口汇总表

---
+++ realtime-common/src/main/java/com/lxj/gmall/realtime/common/bean/TrafficHomeDetailPageViewBean.java
@@ -0,0 +1,28 @@
+package com.lxj.gmall.realtime.common.bean;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+/**
+ * @author Laoxingjie
+ * @description TODO
+ * @create 2026/4/11 13:52
+ **/
+
+@Data
+@AllArgsConstructor
+public class TrafficHomeDetailPageViewBean {
+    // 窗口起始时间
+    String stt;
+    // 窗口结束时间
+    String edt;
+    // 当天日期
+    String curDate;
+    // 首页独立访客数
+    Long homeUvCt;
+    // 商品详情页独立访客数
+    Long goodDetailUvCt;
+    // 时间戳
+    @JSONField(serialize = false)
+    Long ts;
+}
